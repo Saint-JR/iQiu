be7abd4933ad9f2715dfdf804f087562cba74ba1
Saint.JR <1911619172@qq.com>
2022-12-03 14:23:51 +0800

    v1.0.0 init

---
+++ src/main/java/com/example/iqiu_backend/IQiuBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.iqiu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IQiuBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IQiuBackEndApplication.class, args);
+    }
+
+}
